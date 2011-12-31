7e2c82e6659b0496a1df5c305a3e1ddda1059d6b
Laurent FABRE <laurent_psychedelic@hotmail.com>
2011-12-31 13:41:32 +0900

    Add support for redo+

--- init.el
+++ init.el
@@ -200,6 +200,15 @@
 (define-key global-map (kbd "<f7>") 'point-undo)
 (define-key global-map (kbd "S-<f7>") 'point-redo)
 
+;;;;;;;;;;;
+;; redo+ ;;
+;;;;;;;;;;;
+(require 'redo+)
+(global-set-key (kbd "C-M-_") 'redo)
+(setq undo-no-redo t)
+(setq undo-limit 600000)
+(setq undo-strong-limit 900000)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; which-function-mode ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;

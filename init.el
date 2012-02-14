dae0de491f008ebe07790d628670b82a6f816d5a
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-02-14 09:51:50 +0900

    Add support for window transparency (tested only on WInXP)

--- init.el
+++ init.el
@@ -27,9 +27,20 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:background "black" :foreground "white" :cursor-type "box")))))
+;; '(default ((t (:background "black" :foreground "white" :cursor-type "box")))))
+ '(default ((t (:stipple nil :background "dark blue" :foreground "white" :cursor-type "box")))))
 ;:font "-outline-Courier New-normal-normal-normal-mono-11-*-*-*-c-*-iso8859-1"
 (set-face-attribute 'default nil :height 100)
+;;(set-face-attribute 'default nil :stipple "c:/Documents and Settings/LFabre/My Documents/Downloads/1328746948_crkoinarwhal_442920.jpeg")
+
+;; Set transparency of emacs
+(defun transparency (value)
+  "Sets the transparency of the frame window. 0=transparent/100=opaque"
+  (interactive "nTransparency Value 0 - 100 opaque:")
+  (set-frame-parameter (selected-frame) 'alpha value))
+
+(transparency '80)
+(global-set-key (kbd "C-c t") 'transparency)
 
 ;; locale
 (setenv "LC_TIME" "C")

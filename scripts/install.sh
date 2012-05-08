5d435c4a3bbf84e3bdc9bf16d837e124167d1f51
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-05-08 21:40:11 +0900

    1-Modified the settings installation script in order to work correctly on Win7.
    2-Changed the permissions of bin/junction.exe in order to make it executable (symbolic link on win32).
    
    The installation script add the bin folder location to the path momentarily in order to make the junction executable accessible.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,18 +5,22 @@ SYSTEM=`uname -o`
 if [[ $SYSTEM =~ "Cygwin" ]]
 then
     echo "Windows!"
+
     SETTINGS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     SETTINGS_DIR=$(dirname $SETTINGS_DIR)
     SETTINGS_DIR=`cygpath -w $SETTINGS_DIR`
+
+    PATH=$SETTINGS_DIR/bin:$PATH
+
     HOME_DIR=`cygpath -w ~`
     #HARD LINK TO .BASHRC
     FILE_LNK=$(cygpath -w $HOME_DIR/.bashrc)
     FILE_TAR=$(cygpath -w $SETTINGS_DIR/.bashrc)
-    junction -s $FILE_LNK $FILE_TAR
+    junction -s "$FILE_LNK" "$FILE_TAR"
     #HARD LINK TO .EMACS.D
     FILE_LNK=$(cygpath -w $HOME_DIR/.emacs.d)
     FILE_TAR=$(cygpath -w $SETTINGS_DIR/.emacs.d)
-    junction -s $FILE_LNK $FILE_TAR
+    junction -s "$FILE_LNK" "$FILE_TAR"
 else
     echo "Linux!"
     SETTINGS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

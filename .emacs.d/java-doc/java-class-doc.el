e5d74ba40a7748bd0c052fe96c39db54950bb0aa
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-05-10 16:14:30 +0900

    1-Modified java-class-doc.el to refer to Java 6 documentation and not 1.4.2

--- .emacs.d/java-doc/java-class-doc.el
+++ .emacs.d/java-doc/java-class-doc.el
@@ -5,8 +5,7 @@
 ;; Version: 0.1
 ;; Keywords: java, documentation, api
 ;; Author: Mathias Dahl <mathias.rem0veth1s.dahl@gmail.com>
-;; Maintainer: Mathias Dahl
-;; URL: http://www.emacswiki.org/cgi-bin/wiki/JavaClassDoc
+;; Maintainer: Mathias Dahl;; URL: http://www.emacswiki.org/cgi-bin/wiki/JavaClassDoc
 ;;
 ;; This file is not (yet) part of GNU Emacs.
 ;;
@@ -42,7 +41,7 @@
 ;;  - This is the most important step and what needs manual work.  Get
 ;;    the `allclasses-noframe.html' file, either from a local
 ;;    installation of the Java API documentation or from the internet
-;;    (http://java.sun.com/j2se/1.4.2/docs/api/allclasses-noframe.html)
+;;    (http://docs.oracle.com/javase/6/docs/api/allclasses-noframe.html)
 ;;
 ;;  - Evaluate this file (M-x eval-buffer RET)
 ;;
@@ -98,7 +97,7 @@ specific Java class is documented.")
 
 If you do not have the Java documentation locally, save the web
 page
-http://java.sun.com/j2se/1.4.2/docs/api/allclasses-noframe.html
+http://java.sun.com/j2se/6/docs/api/allclasses-noframe.html
 to a local file and use that file when answering the
 prompt.
 
@@ -154,11 +153,11 @@ After the scan is complete, you probably want to call
             (car (read-from-string
                   (buffer-substring (point-min) (point-max))))))))
 
-(defvar java-class-doc-base-url "http://java.sun.com/j2se/1.4.2/docs/api"
+(defvar java-class-doc-base-url "http://java.sun.com/j2se/6/docs/api"
   "Base URL for looking up Java documentation.
 Can be a local file or a web page.  For a local file, use
 file:///c:\\java\\docs\\api.  Directly from sun.com, use
-http://java.sun.com/j2se/1.4.2/docs/api.  The path should not end
+http://java.sun.com/j2se/6/docs/api.  The path should not end
 in a slash.")
 
 (defun java-class-doc ()

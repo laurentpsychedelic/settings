efd0b91a086be22d0f09c4d08ad46512f98873b1
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-11-26 14:36:24 +0900

    1-Added a function to format XML data (Benjamin Ferrari's blog: http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html).

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -789,6 +789,21 @@
 ;; (run-with-idle-timer 0.5 t 'auto-save-buffers "\\.c$" "^$") ; .c ‚¾‚¯‘ÎÛ
 ;; (run-with-idle-timer 0.5 t 'auto-save-buffers ""   "\\.h$") ; .h ‚¾‚¯œŠO
 
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
+
 ;;;;;;;;;;;;;
 ;; recentf ;;
 ;;;;;;;;;;;;;

244654143a67c708e276e81c6733c51105f56789
Laurent FABRE <laurent_psychedelic@hotmail.com>
2013-01-10 06:53:35 +0900

    1-Fixed settings of solarized-emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -692,7 +692,7 @@
 ;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs/solarized-emacs/")
 (require 'solarized)
-; (add-to-list 'custom-theme-load-path "~/.emacs.d/solarized-emacs/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/solarized-emacs/")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; java documentation ;;
@@ -936,10 +936,11 @@ collect `(define-abbrev ,table
         ("while" "while () {\n}\n" "C-M-b C-M-q C-- C-M-d")
         ("for"   "for (;;) {\n}\n" "C-M-b C-M-q C-M-b C-M-d"))))
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(inhibit-startup-screen t))
 
 ;; eval-and-replace
@@ -949,3 +950,9 @@ collect `(define-abbrev ,table
   (kill-sexp -1)
   (insert (format "%S" value)))
 (global-set-key (kbd "C-x C-r") 'eval-and-replace)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

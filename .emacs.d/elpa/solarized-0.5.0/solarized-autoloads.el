7fd2eed42b98bab265781abf193b6c6a7cc2a1e9
Laurent FABRE <laurent_psychedelic@hotmail.com>
2013-01-10 06:15:18 +0900

    1-Added solarized-emacs package to Emacs (it seems to require Emacs 24...).

---
+++ .emacs.d/elpa/solarized-0.5.0/solarized-autoloads.el
@@ -0,0 +1,24 @@
+;;; solarized-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "solarized" "solarized.el" (20717 56318))
+;;; Generated autoloads from solarized.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("solarized-pkg.el") (20717 56318 625822))
+
+;;;***
+
+(provide 'solarized-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; solarized-autoloads.el ends here
---
+++ .emacs.d/elpa/solarized-0.5.0/solarized-pkg.el
@@ -0,0 +1,1 @@
+(define-package "solarized" "0.5.0" "Solarized for Emacs." (quote nil))

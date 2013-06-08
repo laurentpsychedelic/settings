a8ab9154283c16b04a2a6a41474b4e9b0a05109c
Laurent FABRE <laurent_psychedelic@hotmail.com>
2013-06-09 00:06:01 +0900

    Added a function in myantcompile.el to list all files of a given name and extension in a given directory, recursively (I used gist at https://gist.github.com/dmgerman/5675462). I will use it to get a list of classes to import in Java file.

--- .emacs.d/myantcompile.el
+++ .emacs.d/myantcompile.el
@@ -301,6 +301,71 @@
   (let ((import (thing-at-point 'word)))
     (insert-import-if-not-present-impl import)))
 
+
+;; (defun test-find-all-files-in-dir ()
+;;   ""
+;;   (interactive)
+;;   (let ((files ()))
+;;     (setq files (find-all-files-in-dir "~/dev/design_patterns" "Factory" "java"))
+;;     (mapc (lambda (ele) (message (format "Ele-> %s" ele))) files)))
+
+(defun find-all-files-in-dir (directory filename extension)
+  "Find all files with given name and extension in given directory"
+  (let ((files ()) (tmp ()))
+    (setq tmp (directory-files-recursive directory (concat filename "." extension) 10 "nothing"))
+    (mapc (lambda (ele) (if (string-match (regexp-quote (concat filename "." extension)) ele) (setq files (cons ele files)))) tmp)
+    files))
+
+;;;
+;;; Recursively list files in a given directory
+;;;
+;;; Author:    daniel m german dmg at uvic dot ca
+;;; Copyright: daniel m german
+;;; License:   Same as Emacs
+;;;
+(defun directory-files-recursive (directory match maxdepth ignore)
+  "List files in DIRECTORY and in its sub-directories. 
+   Return files that match the regular expression MATCH but ignore     
+   files and directories that match IGNORE (IGNORE is tested before MATCH. Recurse only 
+   to depth MAXDEPTH. If zero or negative, then do not recurse"
+  (let* ((files-list '())
+         (current-directory-list
+          (directory-files directory t)))
+    ;; while we are in the current directory
+     (while current-directory-list
+       (let ((f (car current-directory-list)))
+         (cond 
+          ((and
+           ignore ;; make sure it is not nil
+           (string-match ignore f))
+           ; ignore
+            nil
+           )
+          ((and
+            (file-regular-p f)
+            (file-readable-p f)
+            (string-match match f))
+          (setq files-list (cons f files-list))
+           )
+          ((and
+           (file-directory-p f)
+           (file-readable-p f)
+           (not (string-equal ".." (substring f -2)))
+           (not (string-equal "." (substring f -1)))
+           (> maxdepth 0))     
+           ;; recurse only if necessary
+           (setq files-list (append files-list (directory-files-recursive f match (- maxdepth -1) ignore)))
+           (setq files-list (cons f files-list))
+           )
+          (t)
+          )
+         )
+       (setq current-directory-list (cdr current-directory-list))
+       )
+       files-list
+     )
+    )
+
 ;; (defun test-show-menu-and-get-selected-element ()
 ;;   "Test function for show-menu-and-get-selected-element"
 ;;   (interactive)

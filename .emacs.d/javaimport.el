99a354ed195793e55fa087a0854bfee8913f4bad
Laurent FABRE <laurent_psychedelic@hotmail.com>
2013-08-31 22:42:14 +0900

    Issue resolved in javaimport.el (see last commit). Removing all comments (AND string litterals!) allowing resolving the issue.

--- .emacs.d/javaimport.el
+++ .emacs.d/javaimport.el
@@ -26,7 +26,9 @@
 
 (setq javaimport-valid-object-name "[a-zA-Z_][a-zA-Z_0-9]*")
 (setq javaimport-package-regexp (concat "package[[:space:]]+\\(" javaimport-valid-object-name "\\([.]" javaimport-valid-object-name "\\)*\\)"))
-(setq javaimport-class-regexp (concat "class[[:space:]]+\\(" javaimport-valid-object-name "\\)"))
+(setq javaimport-class-regexp (concat "\\(\\(public\\)[[:space:]]+\\)?\\(\\(class\\)\\|\\(interface\\)\\)[[:space:]]+\\(" javaimport-valid-object-name "\\)"))
+(setq javaimport-class-regexp-class-name-index 6)
+(setq javaimport-class-regexp-class-access-modifier-index 2)
 
 (defun javaimport-compute-brace-differential (text)
   "Compute the differential of right - left curved braces in given text"
@@ -41,7 +43,7 @@
     (let ((n 0))
       (setq case-fold-search nil)
       (insert text)
-      (goto-char 1)
+      (beginning-of-buffer)
       (while (re-search-forward (regexp-quote pattern) nil t)
         (setq n (1+ n)))
       n)))
@@ -61,8 +63,7 @@ offset=-1, 'AnOtherClass' is returned"
         (setq parent-eles (split-string parent-class "[.]")) ; elements of the parent class name
         (setq npop (1+ (- offset))) ; number of elements to pop from parent class before appending sub-class elements
         (mapc (lambda (ele) (pop parent-eles)) (make-list npop 1))
-        (setq sub-class (mapconcat 'identity (append parent-eles (cons sub-class '())) "."))
-        ))
+        (setq sub-class (mapconcat 'identity (append parent-eles (cons sub-class '())) "."))))
   sub-class)
 
 (defun javaimport-scan-package-in-source (source-code)
@@ -70,20 +71,49 @@ offset=-1, 'AnOtherClass' is returned"
   (string-match javaimport-package-regexp source-code)
   (match-string 1 source-code))
 
+(defun javaimport-remove-all-comments-in-buffer ()
+  "Remove all comments in current buffer"
+  (interactive)
+  (beginning-of-buffer)
+  (while ; // comments
+      (re-search-forward (regexp-quote "//") nil t)
+    (move-beginning-of-line nil)
+    (kill-line))
+  (beginning-of-buffer)
+  (while ; /* ... */ comments
+      (re-search-forward (regexp-quote "/*") nil t)
+    (goto-char (match-beginning 0))
+    (push-mark-command nil)
+    (re-search-forward (regexp-quote "*/"))
+    (kill-region (point) (mark))))
+
+(defun javaimport-remove-all-string-litterals-in-buffer ()
+  "Remove all comments in current buffer"
+  (interactive)
+  (beginning-of-buffer)
+  (while (re-search-forward (regexp-quote "\"") nil t)
+    (goto-char (match-beginning 0))
+    (push-mark-command nil)
+    (forward-char)
+    (re-search-forward (regexp-quote "\""))
+    (kill-region (point) (mark))))
+
 (defun javaimport-test-scan-defined-classes-in-source ()
   "Test..."
   (interactive)
-  (javaimport-scan-defined-classes-in-source (buffer-string)))
+  (message (format "Classes: %s" (javaimport-scan-defined-classes-in-source (buffer-string)))))
 (defun javaimport-scan-defined-classes-in-source (source-code)
   "Scan source code and return a list of the classes defined within it"
   (with-temp-buffer
     (let ((package "") (class-list ()) (curr-class nil) (last-class nil) (class-offset 0) (curr-point 1) (last-point 1) (interval-text ""))
       (insert source-code)
-      (goto-char 0)
+      (javaimport-remove-all-comments-in-buffer)
+      (javaimport-remove-all-string-litterals-in-buffer)
+      (beginning-of-buffer)
       (setq case-fold-search nil)
       (setq package (javaimport-scan-package-in-source source-code))
       (while (setq curr-point (re-search-forward javaimport-class-regexp nil t))
-        (setq curr-class (match-string 1))
+        (setq curr-class (match-string javaimport-class-regexp-class-name-index))
         (setq interval-text (substring source-code last-point curr-point))
         (setq class-offset (javaimport-compute-brace-differential interval-text))
         (setq curr-class (javaimport-combine-sub-class-with-parent-class last-class curr-class class-offset))
@@ -92,7 +122,6 @@ offset=-1, 'AnOtherClass' is returned"
         (setq last-point curr-point))
       (setq class-list (mapcar (lambda (ele) (if package (concat package "." ele) ele)) class-list))
       class-list)))
-
 
 (provide 'javaimport)
 ;;; javaimport.el ends here
